d95135db128ef97eb9b95cc369d97844ae67790c
Hamza <hamzamalik5468@gmail.com>
2026-09-18 16:50:38 +0500

    Complete counter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,26 @@
-import { useState } from "react";
-
+import {useState} from "react";
 
 function App() {
-  const [backgroundColor, setBackgroundColor] = useState("White");
-  return (
-    <div
-      style={{
-        BackgroundColor: backgroundColor,
-        minHeight: "100px",
-        padding: "20px",
-      }}
-    >
+  const [count, setCount] = useState(0);
 
-      <h1
-        style={{ color: "black" }}>
-        Color Change
-      </h1>
+    return (
+      <div>
+      <h1>Counter App</h1>
+      <h2>Count: {count}</h2>
 
-      <p style={{ color: "black" }}>
-        Current color: {backgroundColor}
-      </p>
-      <button onClick={() => setBackgroundColor("lightblue")}>
-        Blue
+      <button onClick={()=>setCount(count+1)}>
+        Increase
       </button>
 
-      <button onClick={() => setBackgroundColor("lightgreen")}>
-        Green
+      <button onClick={() => setCount(count > 0 ? count - 1 : 0 )}>
+        decrease
       </button>
 
-      <button onClick={() => setBackgroundColor("lightpink")}>
-        pink
-      </button>
-
-      <button onClick={() => setBackgroundolor ("White")}>
+      <button onClick= {()=> setCount (0)}>
         Reset
       </button>
-
-    </div>
-
-  );
+      </div>
+    );
 }
 
 export default App;
